3c18119950c3a198bf5ad9a304cbe58cc8508d40
Jared Schwartz <jareds1070@gmail.com>
2024-10-17 22:27:58 -0500

    push

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// TODO: this file! :)
+const addNumberBtn = document.getElementById("add-num-btn");
+const numberInput = document.getElementById("number-input");
+const numberBank = document.getElementById("numberBank");
+const numberBankOutput = document.querySelector("#numberBank > output");
+
+addNumberBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    const number = numberInput.value;
+    numberBankOutput.textContent = number;
+});
+
+
+
